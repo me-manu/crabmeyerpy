2c57fa8fd8d70f5208773b6fdbdfce7b38cb7313
Tim Unbehaun <tim.unbehaun@fau.de>
2021-08-20 20:49:39 +0200

    changed gwind_max to super exp cutoff

--- crabmeyerpy/electrondensity.py
+++ crabmeyerpy/electrondensity.py
@@ -22,6 +22,37 @@ def nel_crab(gamma, **params):
 
     # Wind electron spectrum
     m_wind = (gamma > params['gmin']) & (gamma <= 1. / params['gwind_b'])
+    m_wind_br = (gamma > 1. / params['gwind_b']) & (gamma < params['gmax'])
+    m_wind_tot = (gamma > params['gmin']) & (gamma < params['gmax'])
+    
+    result[m_wind] += np.power(gamma[m_wind] * params['gwind_b'], params['Swind'])
+    result[m_wind_br] += np.power(gamma[m_wind_br] * params['gwind_b'], params['Swind'] + params['Sbreak'])
+    result[m_wind_tot] *= params['Nwind']
+    result[m_wind_tot] *= np.exp(-np.power(params['gwind_min'] / gamma[m_wind_tot], params['sup_wind']))
+    result[m_wind_tot] *= np.exp(- np.power(gamma[m_wind_tot]/ params['gwind_max'],2))  # exp cutoff after gwind_max
+    # Radio electron spectrum
+    m_radio = (gamma > params['gradio_min']) & (gamma < params['gradio_max'])
+    result[m_radio] += np.power(gamma[m_radio], params['Sradio']) * params['Nradio']
+    return result
+
+def nel_crab_hardcutoff(gamma, **params):
+    """
+    Computes total electron number spectrum per unit gamma interval dN / dgamma
+    as used for crab nebula
+
+    gamma: array-like
+        gamma factors
+    params: dict 
+        function parameters
+
+    Returns
+    -------
+    Total electron spectrum as array
+    """
+    result = np.zeros(gamma.shape)
+
+    # Wind electron spectrum
+    m_wind = (gamma > params['gmin']) & (gamma <= 1. / params['gwind_b'])
     m_wind_br = (gamma > 1. / params['gwind_b']) & (gamma < params['gwind_max'])
     m_wind_tot = (gamma > params['gmin']) & (gamma < params['gwind_max'])
     result[m_wind] += np.power(gamma[m_wind] * params['gwind_b'], params['Swind'])
